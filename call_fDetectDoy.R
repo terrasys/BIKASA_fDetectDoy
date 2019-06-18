8139b1dbb8cb28ab974d86b789f22986af9968f1
Markus Möller <markus.moeller@terrasys.info>
2019-06-18 16:57:13 +0200

    Create call_fDetectDoy.R

---
+++ call_fDetectDoy.R
@@ -0,0 +1,41 @@
+#-----------------------------------------------------------------------------------------------------
+
+#-----------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------
+print("Working directory, data and settings")
+#-----------------------------------------------------------------------------------------------------
+source(file.path(FUNC.DIR,"fPackages.R"))
+source(file.path(FUNC.DIR,"fDetectDoy.R"))
+TS.SHP = "d:/Dropbox/_git/BIKASA_DetectDoy/_input/ST_LO_epsg31468.shp"
+PP.DIR = "d:/Dropbox/_git/BIKASA_DetectDoy/_input/"
+OUT.DIR = "d:/Dropbox/_git/BIKASA_DetectDoy/_output/"
+FUNC.DIR = "d:/Dropbox/_git/BIKASA_DetectDoy/_functions/"
+YEARS=1993:2018
+
+PLANT=202#Winter Wheat
+PHASES=c(10,12,15,18,19,21,24)
+fDetectTempSum(TS.SHP=TS.SHP,
+               PP.DIR=PP.DIR,
+               OUT.DIR=OUT.DIR,
+               YEARS=YEARS,
+               PLANT=PLANT,
+               PHASES=PHASES)
+
+#PLANT=205#Winter Rapeseed
+#PHASES=c(10,12,14,67,17,5,22,24)
+#fDetectTempSum(TS.SHP=TS.SHP,
+#               PP.DIR=PP.DIR,
+#               OUT.DIR=OUT.DIR,
+#               YEARS=YEARS,
+#               PLANT=PLANT,
+#               PHASES=PHASES)
+
+PLANT=215#Maize
+PHASES <- c(10,12,67,5,65,19,20,21,24)
+fDetectTempSum(TS.SHP=TS.SHP,
+               PP.DIR=PP.DIR,
+               OUT.DIR=OUT.DIR,
+               YEARS=YEARS,
+               PLANT=PLANT,
+               PHASES=PHASES)
+
